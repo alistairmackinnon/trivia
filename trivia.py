942b7e07bb7ee58d6c735d1e730e630cab0e299c
Ali <alistair.mackinnon@outlook.com>
2020-02-15 13:15:08 +0000

    function get question

--- trivia.py
+++ trivia.py
@@ -2,27 +2,17 @@ import requests
 import base64
 
 
-def get_questions():
+def get_questions(amount):
     request_url = f'https://opentdb.com/api.php?amount={amount}&type=boolean&encode=base64'
     response = requests.get(request_url)
     response_lst = response.json()
-    return response_lst['results']
+    questions = response_lst['results']
+    for question_detail in questions:
+        question = question_detail['question']
+        question = base64.b64decode(question).decode('ascii')
+        correct_answer = question_detail['correct_answer']
+        correct_answer = base64.b64decode(correct_answer).decode('ascii')
+        return question, correct_answer
 
-
-print('How many questions would you like?')
-amount = input()
-
-questions = get_questions()
-
-for question_detail in questions:
-    question = question_detail['question']
-    question = base64.b64decode(question).decode('ascii')
-    correct_answer = question_detail['correct_answer']
-    correct_answer = base64.b64decode(correct_answer).decode('ascii')
-    print('True or False?')
-    print(question)
-    user_answer = input()
-    if user_answer == correct_answer:
-        print('Correct')
-    else:
-        print(f'Incorrect - the answer is {correct_answer}')
+if __name__ == "__main__":
+    get_questions(1)
