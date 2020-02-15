cbebc1157967b349584f0319cea3581b3d34f97d
Ali <alistair.mackinnon@outlook.com>
2020-02-15 18:52:33 +0000

    frame mplemeted

--- gui.py
+++ gui.py
@@ -5,6 +5,9 @@ import trivia
 window = Tk()
 window.title("Trivia App")
 
+frame = Frame(window)
+frame.pack()
+
 
 # Functions
 def validate_answer():
@@ -22,24 +25,24 @@ question = question_tup[0]
 answer = question_tup[1]
 
 # Set Gui
-ques_txt = Label(window, wraplength=300, text=question)
+ques_txt = Label(frame, wraplength=300, text=question)
 ques_txt.grid(column=0, row=0, columnspan=2, padx=20, pady=20)
 
 # Radio Buttons
 selected = StringVar()
-rad1 = Radiobutton(window, text='True', value="True", variable=selected)
-rad2 = Radiobutton(window, text='False', value="False", variable=selected)
+rad1 = Radiobutton(frame, text='True', value="True", variable=selected)
+rad2 = Radiobutton(frame, text='False', value="False", variable=selected)
 rad1.grid(column=0, row=1)
 rad2.grid(column=1, row=1)
 
 # Submit Button
 submit_txt = StringVar()
 submit_txt = 'Submit'
-btn = Button(window, text=submit_txt, command=validate_answer)
+btn = Button(frame, text=submit_txt, command=validate_answer)
 btn.grid(column=0, row=2)
 
 # Quit Button
-quit_btn = Button(window, text="Quit", command=window.destroy)
+quit_btn = Button(frame, text="Quit", command=window.destroy)
 quit_btn.grid(column=1, row=2)
 
 
