ef25f5a8ead8b7bde373f21322ba7a06b787fe44
Ali <alistair.mackinnon@outlook.com>
2020-02-16 22:17:55 +0000

    implementing class

--- gui.py
+++ gui.py
@@ -1,48 +1,65 @@
 from tkinter import *
-from tkinter.ttk import *
 import trivia
 
-window = Tk()
-window.title("Trivia App")
 
-frame = Frame(window)
-frame.pack()
+class Window(Frame):
+    def __init__(self, master=None):
+        self.answer = StringVar()
+        self.user_answer = StringVar()
+        self.selected = StringVar()
+
+        Frame.__init__(self, master)
+        self.master = master
+        self.init_window()
+
+    def validate_answer(self):
+        user_answer = self.selected.get()
+        if user_answer == self.answer:
+            print('Correct')
+        else:
+            print('Incorrect')
+
+    def client_exit(self):
+        exit()
+
+    # Creation of init_window
+    def init_window(self):
+        # changing the title of our master widget
+        self.master.title("GUI")
+
+        # Set question context
+        question_tup = trivia.get_questions(1)
+        question = question_tup[0]
+        self.answer = question_tup[1]
+
+        # allowing the widget to take the full space of the root window
+        self.pack(fill=BOTH, expand=1)
+
+        # creating a button instance
+        quit_button = Button(self, text="Quit", command=self.client_exit)
+
+        # placing the button on my window
+        quit_button.grid(column=1, row=2)
+
+        ques_txt = Label(self, wraplength=300, text=question)
+        ques_txt.grid(column=0, row=0, columnspan=2, padx=20, pady=20)
+
+        # Radio Buttons
+        rad1 = Radiobutton(self, text='True', value="True", variable=self.selected)
+        rad2 = Radiobutton(self, text='False', value="False", variable=self.selected)
+        rad1.grid(column=0, row=1)
+        rad2.grid(column=1, row=1)
+
+        # Submit Button
+        submit_txt = 'Submit'
+        btn = Button(self, text=submit_txt, command=self.validate_answer)
+        btn.grid(column=0, row=2)
 
 
-# Functions
-def validate_answer():
-    global submit_txt
-    user_answer = selected.get()
-    if user_answer == answer:
-        print('Correct')
-    else:
-        print('Incorrect')
+root = Tk()
 
+# size of the window
+root.geometry("400x300")
 
-# Set question context
-question_tup = trivia.get_questions(1)
-question = question_tup[0]
-answer = question_tup[1]
-
-# Set Gui
-ques_txt = Label(frame, wraplength=300, text=question)
-ques_txt.grid(column=0, row=0, columnspan=2, padx=20, pady=20)
-
-# Radio Buttons
-selected = StringVar()
-rad1 = Radiobutton(frame, text='True', value="True", variable=selected)
-rad2 = Radiobutton(frame, text='False', value="False", variable=selected)
-rad1.grid(column=0, row=1)
-rad2.grid(column=1, row=1)
-
-# Submit Button
-submit_txt = 'Submit'
-btn = Button(frame, text=submit_txt, command=validate_answer)
-btn.grid(column=0, row=2)
-
-# Quit Button
-quit_btn = Button(frame, text="Quit", command=window.destroy)
-quit_btn.grid(column=1, row=2)
-
-# Launch Window
-window.mainloop()
+app = Window(root)
+root.mainloop()
